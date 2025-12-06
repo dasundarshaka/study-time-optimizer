f1dca04dbdddc2aa3e9a196a8d560af0b6d8aace
dasundarshaka <dasun.darshaka01@gmail.com>
2025-12-06 18:55:36 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,16 +1,7 @@
-﻿import { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8081',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
+  base: '/study-time-optimizer/',  // Add this line with your repo name
 })
